6bfcb8ae5b1fbf35c9920debebc4671e68eca133
Larissa-Alves-de-Oliveira <alvesoliveira977@gmail.com>
2023-06-13 07:42:46 -0300

    banco

--- banco/lista5.sql
+++ banco/lista5.sql
@@ -1,16 +1,16 @@
 create database lista5;
-create table agenda (
-	id_agenda int not null auto_increment,
-    nome varchar(50) not null,
-    apelido varchar(40) not null,
-    endereco varchar(70) not null,
+
+create table agenda(
+	id_agenda int primary key not null auto_increment,
+	nome varchar(50) not null,
+	apelido varchar(40) not null,
+	endereco varchar(70) not null,
     bairro varchar(50) not null,
     cidade varchar(50) not null,
-    estado varchar(02) not null,
+    estado varchar(2) not null,
     telefone varchar(15) not null,
     celular varchar(15) not null,
     email varchar(70) not null,
     dt_cadastro date not null,
-    foto varchar(100) null
-    primary key (id_agenda)
-);
+    foto varchar(50));
+    
